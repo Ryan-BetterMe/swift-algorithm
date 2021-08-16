7d20c1f6ce09d7287ee251c4aa8ef23d30e49b34
Ryan <lancexianghui@gmail.com>
2021-08-16 22:54:24 +0800

    🤗 字符串：最长回文字符串的中心扩散法

--- LeetCode_Solution/LeetCode_Solution/2.String/2_LongestPalindrome.swift
+++ LeetCode_Solution/LeetCode_Solution/2.String/2_LongestPalindrome.swift
@@ -29,12 +29,91 @@
 import Foundation
 
 /// 暴力破解法，这种方法的时间复杂度是O(n^2)
+func isPalindrome(_ s: String) -> String {
 
+    guard s.count > 1 else { return s }
 
-/// 暴力破解法优化
+    let array = Array(s)
+    var maxStr = ""
+    
+    for i in 0..<s.count {
+        for j in i..<s.count {
+            let str = Array(array[i...j]).map { "\($0)" }.joined(separator: "")
+            
+            if judgePalindromeString(str) && str.count > maxStr.count {
+                maxStr = str
+            }
+        }
+    }
+    return maxStr
+}
 
+/// 判断是否为回文字符串
+func judgePalindromeString(_ s: String) -> Bool {
+    let array = Array(s)
+    
+    var i = 0
+    var j = array.count - 1
+    
+    while i < j {
+        if array[i] != array[j] {
+            return false
+        } else {
+            i += 1
+            j -= 1
+        }
+    }
+    
+    return true
+}
 
 /// 中心字符拓展法
+func longestPalindrome2(_ s: String) -> String {
+    guard s.count > 1 else { return s }
+    
+    let array = Array(s)
+    var maxStr = "\(array[0])"
+    
+    for index in 0..<array.count - 1 {
+        var odd = ""
+        var even = ""
+        
+         // 奇数：以自己为中心向外拓张
+        odd = maxCenterPalindrome(array, index, index)
+    
+         // 偶数：以自己和下一个元素一起为中心向外拓张
+        if index + 1 < array.count && array[index] == array[index + 1] {
+            even = maxCenterPalindrome(array, index, index + 1)
+        }
+        
+        let max = odd.count >= even.count ?  odd : even
+        
+        if maxStr.count < max.count {
+            maxStr = max
+        }
+    }
+    
+    return maxStr
+}
+
+func maxCenterPalindrome(_ s: Array<Character>, _ left: Int, _ right: Int) -> String {
+    let count = s.count
+    var i = left
+    var j = right
+    
+    while i >= 0 && j <= count - 1 {
+        if s[i] == s[j] {
+            i -= 1
+            j += 1
+        } else {
+            break
+        }
+    }
+    
+    // 这个时候的回文字符串呢？
+    return Array(s[(i+1)..<j]).map { "\($0)" }.joined()
+}
+
 
 
 /// 马拉车算法
