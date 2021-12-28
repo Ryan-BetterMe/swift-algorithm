e5b92feba42aed178448f188e277d117971d2492
Ryan <lancexianghui@gmail.com>
2021-12-28 11:53:53 +0800

    🤗二分法：完成二分法寻找不重复旋转数组的最小值

--- LeetCode_Solution/LeetCode_Solution/3.BinarySearch/4_findMininumValue.swift
+++ LeetCode_Solution/LeetCode_Solution/3.BinarySearch/4_findMininumValue.swift
@@ -7,20 +7,30 @@
 
 import Foundation
 
+// 通过二分法寻找旋转数组的最小值
 func findMin(_ nums: [Int]) -> Int {
-    // 1、找到转折点
-    var center = 0
-    var i = 1
+    // 这里的核心就是left和right
+    // 也就是说我的比较其实是以left或者right为锚点的！！！
+    // 左侧的顺序是不确定的，但是右侧肯定是升序，所以这里我以右侧作为锚点！！
     
-    while i < nums.count {
-        if nums[i] >= nums[i-1] {
-            i += 1
-        } else {
-            center = i
-            break
+    var left = 0
+    var right = nums.count - 1
+    
+    while left < right {
+        let mid = (left + right)/2
+        
+        if nums[mid] == nums[right] {
+            
+            return nums[mid]
+            
+        } else if nums[mid] > nums[right] {
+            
+            left = mid + 1
+            
+        } else if nums[mid] < nums[right] {
+            
+            right = mid
         }
     }
-    
-    // 2、找到最小值
-    return nums[center]
+    return nums[left]
 }
